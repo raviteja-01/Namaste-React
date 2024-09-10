375269d6dc1ee45349fd47811dc46d95d7e54def
Ravi Teja <nagisetti09@gmail.com>
2024-09-10 22:08:03 +0530

    Deleted RestContainer Component file and added state varaible

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,32 @@
 import Search from "./Search";
-import RestoContainer from "./RestoContainer";
+import RestoCard from "./RestoCard";
+import resArray from "../utilis/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resArray);
   return (
     <div className="body">
-      <Search />
-      <RestoContainer />
+      {/* <Search /> */}
+      <div className="filter-btn-container">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (restaraunt) => restaraunt.info.avgRating > 4.3
+            );
+            console.log(filteredList);
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="resto-container">
+        {listOfRestaurants.map((restuarant) => (
+          <RestoCard key={restuarant.info.id} restInfo={restuarant} />
+        ))}
+      </div>
     </div>
   );
 };
